12850941a4a83f3d48a1ad695d485e631a6bea8a
Baptiste Langlade <baptouuuu@gmail.com>
2019-12-05 18:04:17 +0100

    write all the content to the console as ascii content

---
+++ src/Environment/WriteAsASCII.php
@@ -0,0 +1,80 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\CLI\Environment;
+
+use Innmind\CLI\{
+    Environment,
+    Stream,
+};
+use Innmind\Stream\{
+    Readable,
+    Writable
+};
+use Innmind\Url\PathInterface;
+use Innmind\Immutable\{
+    MapInterface,
+    StreamInterface
+};
+
+final class WriteAsASCII implements Environment
+{
+    private $environment;
+    private $output;
+    private $error;
+
+    public function __construct(Environment $environment)
+    {
+        $this->environment = $environment;
+    }
+
+    public function input(): Readable
+    {
+        return $this->environment->input();
+    }
+
+    public function output(): Writable
+    {
+        return $this->output ?? $this->output = new Stream\WriteAsASCII(
+            $this->environment->output()
+        );
+    }
+
+    public function error(): Writable
+    {
+        return $this->error ?? $this->error = new Stream\WriteAsASCII(
+            $this->environment->error()
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function arguments(): StreamInterface
+    {
+        return $this->environment->arguments();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function variables(): MapInterface
+    {
+        return $this->environment->variables();
+    }
+
+    public function exit(int $code): void
+    {
+        $this->environment->exit($code);
+    }
+
+    public function exitCode(): ExitCode
+    {
+        return $this->environment->exitCode();
+    }
+
+    public function workingDirectory(): PathInterface
+    {
+        return $this->environment->workingDirectory();
+    }
+}
