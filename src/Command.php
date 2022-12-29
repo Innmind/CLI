7ff70221f140ad18e419711edaade508c6d18d9c
Baptiste Langlade <baptouuuu@gmail.com>
2022-12-29 11:49:35 +0100

    remove pure annotations as it prevents composition

--- src/Command.php
+++ src/Command.php
@@ -6,9 +6,5 @@ namespace Innmind\CLI;
 interface Command
 {
     public function __invoke(Console $console): Console;
-
-    /**
-     * @psalm-pure
-     */
     public function usage(): string;
 }
